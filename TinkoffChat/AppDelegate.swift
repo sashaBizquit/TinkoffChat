b2e98349984067c80f0641f6ba37b12870c1f229
Александр Лыков <alex.lykov.95@gmail.com>
2018-02-27 16:43:23 +0300

    homework-1: pictures added

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -8,39 +8,59 @@
 
 import UIKit
 
+extension Optional where Wrapped == UIApplicationState {
+     public var description: String {
+        switch self {
+            case .some(.active): return "Active"
+            case .some(.background): return "Background"
+            case .some(.inactive): return "Inactive"
+            case .none: return "Not Running"
+        }
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    private var currentState: UIApplicationState?
+    
+    private func stateChanging(to newStateName: UIApplicationState?, by functionDescription: String) {
+        let actionDescription = UIApplication.shared.applicationState != newStateName ? "is about to move": "has moved"
+        print("Application \(actionDescription) from \(currentState.description) to \(newStateName.description) state: " + functionDescription)
+        currentState = newStateName
     }
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let _window = window {
+            _window.rootViewController = ViewController()
+            _window.makeKeyAndVisible()
+        }
+        stateChanging(to: .inactive, by: #function)
+        return true
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        stateChanging(to: .inactive, by: #function)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        stateChanging(to: .background, by: #function)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        stateChanging(to: .inactive, by: #function)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        stateChanging(to: .active, by: #function)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        stateChanging(to: nil, by: #function)
     }
-
 
 }
 
--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private func viewChanging(with changesDescription: String, by functionDescription: String) {
+        print("View \(changesDescription): " + functionDescription)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .blue
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewChanging(with: "is about to appear", by: #function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        viewChanging(with: "has appeared", by: #function)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        viewChanging(with: "is about to adjust subviews", by: #function)
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        viewChanging(with: "has adjusted subviews", by: #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        viewChanging(with: "is about to disappear", by: #function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        viewChanging(with: "has disappeared", by: #function)
+    }
 
 }
 
